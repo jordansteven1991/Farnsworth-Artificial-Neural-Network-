fd12b84088ebe7f603321d14308aebf2b8146940
jorda <jorda@Stevens-Laptop.attlocal.net>
2022-11-28 14:54:04 -0500

    add teams to cbslookup

--- src/main/java/util/CbsLookup.java
+++ src/main/java/util/CbsLookup.java
@@ -14,7 +14,30 @@ public enum CbsLookup {
 	UCLA("UCLA", "ucla-bruins"),
 	ARK("Arkansas", "arkansas-razorbacks"),
 	PURDUE("Purdue", "purdue-boilermakers"),
-	BAMA("Alabama", "alabama-crimson-tide");
+	BAMA("Alabama", "alabama-crimson-tide"),
+	DUKE("Duke", "duke-blue-devils"),
+	CREIGH("Creighton", "creighton-bluejays"),
+	AUBURN("Auburn", "auburn-tigers"),
+	KANSAS("Kansas", "kansas-jayhawks"),
+	SDGST("San Diego St.", "san-diego-state-aztecs"),
+	OHIOST("Ohio St.","ohio-state-buckeyes"),
+	UNC("North Carolina", "north-carolina-tar-heels"),
+	IOWA("Iowa", "iowa-hawkeyes"),
+	ILL("Illinois", "illinois-fighting-illini"),
+	MARYCA("Saint Mary's", "saint-marys-gaels"),
+	MD("Maryland", "maryland-terrapins"),
+	MICHST("Michigan St.", "michigan-state-spartans"),
+	TXTECH("Texas Tech", "texas-tech-red-raiders"),
+	MEMP("Memphis", "memphis-tigers"),
+	MISSST("Mississippi St.", "mississippi-state-bulldogs"),
+	OKLA("Oklahoma", "oklahoma-sooners"),
+	XAVIER("Xavier", "xavier-musketeers"),
+	PSU("Penn St.", "penn-state-nittany-lions"),
+	VATECH("Virginia Tech", "virginia-tech-hokies"),
+	RUT("Rutgers", "rutgers-scarlet-knights"),
+	OKLAST("Oklahoma St.", "oklahoma-state-cowboys"),
+	MIZZOU("Missouri", "missouri-tigers"),
+	WVU("West Virginia", "west-virginia-mountaineers");
 	
 	private final String key;
     private final String value;
